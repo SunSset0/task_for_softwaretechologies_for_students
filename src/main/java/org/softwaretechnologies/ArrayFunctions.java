5537f77dda720412d9441ea1d049c672136546e0
SunSset0 <145108765+SunSset0@users.noreply.github.com>
2023-10-26 23:42:23 +0300

    Update ArrayFunctions.java

--- src/main/java/org/softwaretechnologies/ArrayFunctions.java
+++ src/main/java/org/softwaretechnologies/ArrayFunctions.java
@@ -7,23 +7,48 @@ public final class ArrayFunctions {
 
     /**
      * Функция, меняющая порядок элементов в массиве array на обратный.
+     *
      * @param array массив, который будет перевернут.
      */
-    public static void reverse(int[] array) {
-        // TODO: реализуйте вышеуказанную функцию
+        public static void reverse(int[] array) {
+            for(int i = 0; i < array.length / 2; i++) {
+                int temp = array[i];
+                array[i] = array[array.length - i - 1];
+                array[array.length - i - 1] = temp;
+            }
     }
 
     /**
      * Функция, заменяющая строки матрицы на столбцы матрицы. Пример:
-       1  2  3     1  4  7
-       4  5  6     2  5  8
-       7  8  9     3  6  9
+     * 1  2  3     1  4  7
+     * 4  5  6     2  5  8
+     * 7  8  9     3  6  9
      * Функция работает только с квадратными матрицами.
      * Если матрица не квадратная, то выведете на экран сообщение:
-       Матрица не квадратная
+     * Матрица не квадратная
+     *
      * @param matrix матрица, в которой столбцы будут заменены на строки.
      */
     public static void rotateMatrix(int[][] matrix) {
-        // TODO: реализуйте вышеуказанную функцию
+        if (matrix.length != matrix[0].length) {
+            System.out.println("Матрица не квадратная");
+            return;}
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = i; j < matrix[0].length; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;}
+        }
+        for (int i = 0; i < matrix.length; i++) {
+            int left = 0;
+            int right = matrix[0].length - 1;
+            while (left < right) {
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+                left++;
+                right--;
+            }
+        }
     }
 }
